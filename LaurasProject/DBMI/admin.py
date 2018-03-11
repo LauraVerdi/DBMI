2eea3f274047f6f5c90ad074909a44528f498b5b
LauraV <lauraverdibello@gmail.com>
2018-03-11 16:09:05 -0400

    removed test projects

--- LaurasProject/DBMI/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# from .models import Question, ParticipantInfo
-from .forms import ParticipantForm
-
-# Register your models here.
-# admin.site.register(Question)
-admin.site.register(ParticipantForm)
-# admin.site.register(Participants)
-

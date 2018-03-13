95ceacdc9b91835a6509b7f92607c676c63ef3eb
LauraV <lauraverdibello@gmail.com>
2018-03-12 21:02:02 -0400

    all set, hopefully the last commit

--- LaurasForms/participants/urls.py
+++ LaurasForms/participants/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.list_participants, name='list_participants'),
-    path('add', views.add_participant, name='add_participant')
+    path('add', views.add_participant, name='add_participant'),
 ]
--- LaurasForms/participants/views.py
+++ LaurasForms/participants/views.py
@@ -1,71 +1,34 @@
-from django.shortcuts import render, get_object_or_404, render
-# from django.http import HttpResponseRedirect
-# from .forms import ParticipantInfoForm
-from .models import ParticipantInfo, ReviewStatus
-from .forms import AddParticipantForm, ReviewStatusForm
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .models import ParticipantInfo, rev_status_choices
+from .forms import AddParticipantForm
 from django.utils import timezone
 from django.shortcuts import redirect
-
-
-# def verify_participant(request, pk):
-#     # participant = ParticipantInfo.objects.filter(pk=pk)
-#     participant = get_object_or_404(ParticipantInfo, pk=pk)
-#     return render(request, 'participants/verify_participant.html', {'participant': participant})
 
 
 def add_participant(request):
     if request.method == "POST":
         form = AddParticipantForm(request.POST)
         if form.is_valid():
-            ParticipantInfo = form.save(commit=False)
-            ParticipantInfo.name = request.user
-            ParticipantInfo.date_created = timezone.now()
-            ParticipantInfo.save()
+            participant = form.save(commit=False)
+            participant.date_created = timezone.now()
+            participant.save()
             return redirect('list_participants')
-        print("in views.add_participant POST")
     else:
         form = AddParticipantForm()
-        print("in else of views.add_participant")
     return render(request, 'participants/add_participant.html', {'form': form})
+
+
+def update_rev_status(name, new_rev_status):
+    ParticipantInfo.objects.filter(participant_name__icontains=name).update(rev_status=new_rev_status)
+    return
 
 
 def list_participants(request):
     participant = ParticipantInfo.objects.all()
-    return render(request, 'participants/list_participants.html', {'participant': participant})
-#
-#
-#
-# def input(request, participant_id):
-#     participant = get_object_or_404(ParticipantInfo, pk=participant_id)
-#     try:
-#         participant.
-#         selected_choice = participant.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the question voting form.
-#         return render(request, 'polls/detail.html', {
-#             'question': question,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    if request.method == "POST":
+        # m = ParticipantInfo(request.POST)
+        for name in ParticipantInfo(request.POST).participant_name:
+            if name != 'csrfmiddlewaretoken':
+                update_rev_status(name, request.POST.get(name))
 
-#     if request.method == 'POST':
-#         form = ParticipantInfoForm(request.POST)
-#         if form.is_valid():
-#             pass
-#         else:
-#             form = ParticipantInfoForm()
-#         return render(request, 'info.html', {'form': form})
-#
-# # def list(request):
-# #     response
-
-def review_status(request):
-    form=ReviewStatusForm()
-    return render(request, 'participants/list_participants.html', {'form': form})
+    return render(request, 'participants/list_participants.html', {'participant': participant, 'choices': rev_status_choices})
